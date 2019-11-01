48df6868f271a3a6adf810813226fb709699d8a2
Giuliano Bortoloni <giulianoagria@hotmail.com>
2019-11-01 18:02:09 -0300

    first commit

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import "./app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css"
+import React from "react";
+import Map from "../components/map/map";
+
+export default props => (
+  <div className="container">
+    <Map></Map>
+  </div>
+);
